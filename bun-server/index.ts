6bd8f4cdc4e9ee2e61414dee1c114aaa831f9966
Mayk <maycon.lrgo@gmail.com>
2023-09-10 12:51:36 -0300

    feat: bun native server

--- bun-server/index.ts
+++ bun-server/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+  port: 3000,
+  fetch() {
+    return new Response("Primeiro código Bun!");
+  },
+});
+
+console.log(`Listening on port ${server.port}`);
